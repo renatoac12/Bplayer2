cda2a888968e49699d896e2bb09fdb062c83148b
RenatoAC0x <renatoarratia54@gmail.com>
2023-10-26 03:01:07 -0300

    commit2

--- Bplayer/settings.py
+++ Bplayer/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "page/static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [STATIC_DIR,]
 MEDIA_URL = "media/"
 
 
