7e65d8fcad53fddb2b73e575d155e60730eba445
RenatoAC0x <renatoarratia54@gmail.com>
2023-10-26 02:43:27 -0300

    commit

---
+++ page/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home),
+
+    path('signin', views.signIn),
+    path('login', views.logIn),
+
+    path('inicio', views.inicio),
+
+    path('torneos', views.torneos),
+    path('partidos', views.partidos),
+
+]
---
+++ page/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def signIn(request):
+    return render(request, 'registro.html')
+
+def logIn(request):
+    return render(request, 'login.html')
+
+def inicio(request):
+    return render(request, 'pagInicial.html')
+
+def torneos(request):
+    return render(request, 'torneos.html')
+
+def partidos(request):
+    return render(request, 'partidos.html')
